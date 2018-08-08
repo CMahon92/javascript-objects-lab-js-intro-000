2faf0a378c4eeaf21b96b8f48f1b493d75de6a7f
Carleen mahon <carleenmahon92@gmail.com>
2018-08-08 23:28:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-return Object.assign({}, object, key);
+var clone = Object.assign({}, [object]: key);
+delete clone.key;
+return clone;
 
 }
 
